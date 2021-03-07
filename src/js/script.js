7dbe814f9f0279a38b3242861fb00dde435082b5
Ivan Lazarev <lazarevivan777@gmail.com>
2021-03-07 19:18:05 +0500

    add mailer script

--- src/js/script.js
+++ src/js/script.js
@@ -100,4 +100,22 @@ $(document).ready(function () {
     // Mask
 
     $('input[name=userPhone]').mask("+7 (999) 999-99-99");
+
+
+    // Mailer
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    })
 });
