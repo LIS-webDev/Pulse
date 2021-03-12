29df1c8448c0175adae06b0a388bffcae8b377ef
Ivan Lazarev <lazarevivan777@gmail.com>
2021-03-12 16:25:50 +0500

    optimize code v1.0

--- src/js/script.js
+++ src/js/script.js
@@ -133,7 +133,7 @@ $(document).ready(function () {
     // Smooth scroll
 
     $(function () {
-        $("a[href^='#']").click(function () {
+        $("a[href=#up]").click(function () {
             var _href = $(this).attr("href");
             $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
             return false;
